a93a05af6b5736e86ab62d832b2f6738c0ed9c6e
tin_pham <tindeptrai1@gmail.com>
2023-08-30 22:55:35 +0700

    style: optimize view for restaurants

--- app/dashboard/(routes)/restaurants/page.tsx
+++ app/dashboard/(routes)/restaurants/page.tsx
@@ -2,12 +2,8 @@
 
 import JsonView from "@/components/json-view";
 import {
-	Avatar,
-	AvatarFallback,
-	AvatarImage,
 	Button,
 	Card,
-	CardContent,
 	CardDescription,
 	CardFooter,
 	CardHeader,
@@ -23,23 +19,32 @@ const RestaurantsPage: React.FC<Props> = ({}) => {
 	const { restaurants } = useGetRestaurants();
 	return (
 		<div className="p-4">
-			<h3 className="text-3xl fw-bold">Restaurants:</h3>
+			<div className="flex justify-start md:justify-between">
+				<h3 className="text-3xl font-bold leading-none tracking-tight">
+					Nhà hàng
+				</h3>
+				<Button>Create Restaurant</Button>
+			</div>
 			<div className="mt-2">
 				<div className="justify-center gap-6 rounded-lg p-4 grid grid-cold-2 md:grid-cols-3 lg:grid-cols-4">
 					{restaurants.map((restaurant) => (
 						<Card key={restaurant.id}>
 							<CardHeader>
-								<CardTitle>{restaurant.name}</CardTitle>
+								<CardTitle
+									style={{
+										height: 48,
+										display: "-webkit-box",
+										overflow: "hidden",
+										lineHeight: "24px",
+										WebkitBoxOrient: "vertical",
+										WebkitLineClamp: 2,
+									}}
+								>
+									{restaurant.name}
+								</CardTitle>
 								<CardDescription>{restaurant.description}</CardDescription>
 							</CardHeader>
-							<CardContent>
-								<Avatar className="w-full rounded-sm">
-									<AvatarImage alt={`image ${restaurant.name}`} />
-									<AvatarFallback className="rounded-sm">
-										{restaurant.name.slice(0, 2)}
-									</AvatarFallback>
-								</Avatar>
-							</CardContent>
+							{/* <CardContent></CardContent> */}
 							<CardFooter className="flex justify-end">
 								<Link href={`/dashboard/${restaurant.id}`}>
 									<Button>Go to detail</Button>
