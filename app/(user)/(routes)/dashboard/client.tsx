3eb6bfc14cc77e1e450b11f137ef95a3ebfcb8a8
tin_pham <tin_pham@datahouse.com>
2024-03-31 16:00:33 +0700

    feat(dashboard): list group donations

--- app/(user)/(routes)/dashboard/client.tsx
+++ app/(user)/(routes)/dashboard/client.tsx
@@ -12,8 +12,10 @@ import {
   CardTitle,
   Heading,
 } from '@/components/ui';
+import { useGetListDonations } from '@/queries/donations';
 import { useGetGroupOrders } from '@/queries/group-orders';
 import { GroupOrder } from '@/queries/group-orders/types';
+import { formatMoney } from '@/utils';
 import Link from 'next/link';
 import React from 'react';
 
@@ -21,6 +23,8 @@ interface Props {}
 
 const Client: React.FC<Props> = ({}: Props) => {
   const { groupLists } = useGetGroupOrders();
+
+  const { listDonations } = useGetListDonations();
 
   const getListCurrentUser = (orders: GroupOrder['Orders']) => {
     if (!groupLists) return [];
@@ -110,6 +114,60 @@ const Client: React.FC<Props> = ({}: Props) => {
               <CardFooter className="flex justify-end">
                 <Link href={`/order/${group.Store.storeSlug}/group-order/${group.id}`}>
                   <Button>Go to order</Button>
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <h3 className="text-xl font-bold">Group Donations</h3>
+        {listDonations?.length === 0 && (
+          <div className="my-10">
+            <h3 className="text-xl font-semibold text-gray-500 text-center">
+              There is no group donations available now.
+            </h3>
+          </div>
+        )}
+
+        <div className="justify-center gap-6 rounded-lg p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {listDonations?.map((group) => (
+            <Card key={group.id}>
+              <CardHeader>
+                <CardTitle
+                  style={{
+                    height: 72,
+                    display: '-webkit-box',
+                    overflow: 'hidden',
+                    lineHeight: '24px',
+                    WebkitBoxOrient: 'vertical',
+                    WebkitLineClamp: 3,
+                  }}
+                  title={group.title}
+                >
+                  {group.title}
+                </CardTitle>
+                <CardDescription
+                  style={{
+                    height: 48,
+                    display: '-webkit-box',
+                    overflow: 'hidden',
+                    lineHeight: '24px',
+                    WebkitBoxOrient: 'vertical',
+                    WebkitLineClamp: 2,
+                  }}
+                  title={group.title}
+                >
+                  {group.description}
+                </CardDescription>
+                <CardDescription>
+                  Donation Target: {formatMoney(group.donationTarget as number)}
+                </CardDescription>
+              </CardHeader>
+              <CardFooter className="flex justify-end">
+                <Link href={`/donation/${group.id}`}>
+                  <Button>Go to donation</Button>
                 </Link>
               </CardFooter>
             </Card>
